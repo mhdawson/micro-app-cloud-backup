c182d53a5f3546605529e1fd5d7c731fac686cfb
Michael Dawson <mdawson@devrus.com>
2016-05-15 11:19:12 -0400

    Add base files

---
+++ server.js
@@ -0,0 +1,3 @@
+// Copyright 2016 the project authors as listed in the AUTHORS file.
+// All rights reserved. Use of this source code is governed by the
+// license that can be found in the LICENSE file.
